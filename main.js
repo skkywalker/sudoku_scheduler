5bcf0ea580895fb0c15eb6ed96aea1c613bb9068
skkywalker <lucas@hehl.com.br>
2020-11-13 11:07:46 -0300

    adiciona funcionalidade de quanto falta coluna

--- main.js
+++ main.js
@@ -1,14 +1,60 @@
 var nomes;
+var tabela = [];
 var ROWS = 10;
+
+function howManyInColumn(nome, column) {
+    var resultado = [];
+    for(var linha = 0; linha < tabela.length; linha++) {
+        if(nome == tabela[linha][column]) {
+            resultado.push(linha.toString() + "-" + column.toString());
+        }
+    }
+    return resultado;
+}
+
+function createTabela() {
+    for(var i = 0; i < ROWS; i++) {
+        tabela[i] = [];
+        for(var j = 0; j < nomes[0][1].length; j++) {
+            tabela[i].push(0);
+        }
+    }
+}
+
+function createFirstRow() {
+    var row_element = document.createElement("tr");
+    $("#app").append(row_element);
+    for(var i = 0; i < nomes[0][1].length; i++) {
+        var element = document.createElement("td");
+        row_element.append(element);
+        var ul = document.createElement("ul");
+        element.append(ul);
+        for(var j = 0; j < nomes.length; j++) {
+            var li = document.createElement("li");
+            var howMany = howManyInColumn(j,i).length;
+            li.innerText = (nomes[j][1][i] - howMany) + "\t" + nomes[j][0];
+            if(nomes[j][1][i] - howMany == 0) {
+                li.style.color = "green";
+            } else {
+                li.style.color = "red";
+            }
+            ul.append(li);
+        }
+    }
+}
 
 function createapp() {
     $("#app").empty();
+    createFirstRow();
     for(var i = 0; i < ROWS; i++) {
         var row_element = document.createElement("tr");
         $("#app").append(row_element);
         for(var j = 0; j < nomes[0][1].length; j++) {
             var element = document.createElement("td");
             var select = document.createElement("select");
+            for(var z = 0; z < nomes.length; z++) {
+                select.append(new Option(nomes[z][0], nomes[z][0]));
+            }
             element.append(select);
             row_element.append(element);
         }
@@ -23,5 +69,6 @@ $("#configForm").submit(function(e){
         nomes[i] = nomes[i].split("-");
         nomes[i][1] = nomes[i][1].split(",");
     }
+    createTabela();
     createapp();
 });
