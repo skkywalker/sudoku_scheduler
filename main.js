257b0626a1c45358a12e8767a4648a9852245805
skkywalker <lucas@hehl.com.br>
2020-11-13 14:58:33 -0300

    finaliza

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 var nomes;
 var tabela = [];
-var ROWS = 10;
-
-function howManyInLine(nome, linha) {
-    var resultado = [];
-    for(var coluna = 0; coluna < tabela[linha].length; coluna++) {
-        if(nome == tabela[linha][coluna]) {
-            resultado.push(linha.toString() + "-" + coluna.toString());
-        }
-    }
-    return resultado;
-}
+var ROWS = 6;
 
 function howManyInColumn(nome, column) {
     var resultado = [];
@@ -28,6 +18,26 @@ function createTabela() {
         for(var j = 0; j < nomes[0][1].length; j++) {
             tabela[i].push(0);
         }
+    }
+}
+
+function checkForDuplicates(line) {
+    var linha = tabela[line];
+    var duplicates = [];
+    for(var i = 0; i < linha.length-1; i++) {
+        for(var j = i+1; j < linha.length; j++) {
+            if(linha[i] == linha[j]) {
+                duplicates.push(line + "-" + i);
+                duplicates.push(line + "-" + j);
+            }
+        }
+    }
+    var unique = [];
+    $.each(duplicates, function(i, el){
+        if($.inArray(el, unique) === -1) unique.push(el);
+    });
+    for(var i = 0; i < unique.length; i++) {
+        $("#"+unique[i]).css("background-color", "red");
     }
 }
 
@@ -53,6 +63,14 @@ function createFirstRow() {
     }
 }
 
+function selected() {
+    var id = this.id.split("-");
+    var row = id[0];
+    var col = id[1];
+    tabela[row][col] = this.value;
+    createapp();
+}
+
 function createapp() {
     $("#app").empty();
     createFirstRow();
@@ -62,12 +80,16 @@ function createapp() {
         for(var j = 0; j < nomes[0][1].length; j++) {
             var element = document.createElement("td");
             var select = document.createElement("select");
+            select.id = i + "-" + j;
+            select.onchange = selected;
             for(var z = 0; z < nomes.length; z++) {
-                select.append(new Option(nomes[z][0], nomes[z][0]));
+                select.append(new Option(nomes[z][0], z));
             }
+            select.value = tabela[i][j];
             element.append(select);
             row_element.append(element);
         }
+        checkForDuplicates(i);
     }
 }
 
